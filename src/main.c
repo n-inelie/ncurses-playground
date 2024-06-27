57d40d26a27a00eaa567c10133dc6750994653eb
n-inelie <nin3li3@yahoo.com>
2024-06-28 01:04:08 +1200

    colors, yeah colors

--- src/main.c
+++ src/main.c
@@ -5,15 +5,22 @@
 
 int main(void) {
     initscr();
+    start_color();
 
     raw();
     keypad(stdscr, true);
     noecho();
 
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+
     attr_on(A_BOLD, NULL);
     PRINT_MIDDLE("Welcome to umm... where again?!", LINES, COLS);
     attr_off(A_BOLD, NULL);
+
+    attr_on(COLOR_PAIR(1), NULL);
     PRINT_MIDDLE_OFFSET("Press any key to continue...", LINES, COLS, 1, 0);
+    attr_off(COLOR_PAIR(1), NULL);
+
     while (1) {
         int ch = getch();
         switch (ch) {
@@ -23,9 +30,11 @@ int main(void) {
                 break;
             default:
                 clear();
+
                 attr_on(A_BOLD, NULL);
                 PRINT_MIDDLE("hello, world", LINES, COLS);
                 attr_off(A_BOLD, NULL);
+
                 refresh();
                 break;
         }
